ec3176da53e93ca23fc0c6133f61d908f97f9f70
cmdmoscati <78150305+cmdmoscati@users.noreply.github.com>
2021-05-23 16:03:09 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,11 +38,10 @@
 						images: {
 							'images/bg01.jpg': 'center',
 							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
 						},
 
 					// Delay.
-						delay: 6000
+						delay: 4000
 
 				};
 
